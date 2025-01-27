f026bc1e7af8d8da627883a453d6a20c66b9f75f
Sira Pornsiriprasert <code@psira.me>
2025-01-27 21:21:24 +0700

    Edit docs

--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,29 @@
  * Copyright 2025 Sira Pornsiriprasert <code@psira.me>
  */
 
+//! # Ellip
 //! **Ellip** is a standalone elliptic integral functions for Rust.
-//!
-//! Ellip is based on Scipy C++ implementations, which were translated from Cephes Math Library.
 //!
 //! # Features
 //! ## Incomplete elliptic integrals
-//! - [ellipf]: Incomplete elliptic integral of the first kind.
-//! - [ellipeinc]: Incomplete elliptic integral of the second kind.
+//! - [fn@ellipf]: Incomplete elliptic integral of the first kind.
+//! - [fn@ellipeinc]: Incomplete elliptic integral of the second kind.
 //! ## Complete elliptic integrals
-//! - [ellipk]: Complete elliptic integral of the first kind.
-//! - [ellipe]: Complete elliptic integral of the second kind.
+//! - [fn@ellipk]: Complete elliptic integral of the first kind.
+//! - [fn@ellipe]: Complete elliptic integral of the second kind.
+//! ## Symmetric elliptic integrals
+//! - [fn@elliprf]: Symmetric elliptic integral of the first kind.
+//! - [fn@elliprd]: Degenerate elliptic integral of the third kind.
+//! - [fn@elliprc]: Degenerate elliptic integral of RF.
+//!
+//! # Acknowledgment
+//! Ellip is derived from multiple mathematic libraries. We thank
+//! the opensource contributors for making mathematic libraries free for all.
+//! Following are the main original works used in the development of Ellip.
+//! Detailed credits are available in the source code.
+//! - [Scipy](https://github.com/scipy/scipy/)
+//! - [Cephes Math Library](https://netlib.org/cephes/)
+//! - [Russell Lab](https://github.com/cpmech/russell)
 //!
 
 mod polyeval;
@@ -28,6 +40,15 @@ mod ellipe;
 pub use ellipe::ellipe;
 mod ellipk;
 pub use ellipk::ellipk;
+
+mod elliprf;
+pub use elliprf::elliprf;
+mod elliprd;
+pub use elliprd::elliprd;
+mod elliprc;
+pub use elliprc::elliprc;
+
+mod constants;
 
 #[cfg(test)]
 mod test_util;
