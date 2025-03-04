7063486d4daa0be030e39dbefcd8a1f83f759257
Sira Pornsiriprasert <code@psira.me>
2025-03-04 17:45:30 +0700

    DOC: Add docs for util and assert_close

--- src/util.rs
+++ src/util.rs
@@ -3,9 +3,14 @@
  * Copyright 2025 Sira Pornsiriprasert <code@psira.me>
  */
 
+//! Utility functions like assert_close.
+
 use num_traits::Float;
 use std::fmt::Debug;
 
+/// Assert that the actual value is within the relative tolerance of the expected value.
+///
+/// Panics if the assertion is failed.
 pub fn assert_close<T: Float + Debug>(expected: T, actual: T, rtol: T) {
     let relative = (actual - expected).abs() / expected;
     if relative > rtol {
