8ec78d979d5afbed02f09dd560d1f21820604faf
Sira Pornsiriprasert <code@psira.me>
2025-04-20 14:10:21 +0700

    DOC: Add plot for ellipdinc

---
+++ examples/ellipd.rs
@@ -0,0 +1,56 @@
+/*
+ * Ellip is licensed under The 3-Clause BSD, see LICENSE.
+ * Copyright 2025 Sira Pornsiriprasert <code@psira.me>
+ */
+
+use ellip::ellipd;
+use plotly::{
+    color::NamedColor,
+    common::{Line, Mode},
+    layout::{Annotation, Axis},
+    ImageFormat, Layout, Plot, Scatter,
+};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let n_points = 100;
+    let range_m = [-2, 1];
+
+    let m: Vec<f64> = (range_m[0] * n_points..=range_m[1] * n_points)
+        .map(|x| x as f64 / n_points as f64)
+        .collect();
+
+    let ellipd_values: Vec<f64> = m.iter().map(|&mi| ellipd(mi).unwrap()).collect();
+
+    let trace = Scatter::new(m, ellipd_values)
+        .mode(Mode::Lines)
+        .name("D(m)")
+        .line(Line::new().color(NamedColor::Red));
+
+    let mut plot = Plot::new();
+    plot.add_trace(trace);
+    plot.set_layout(
+        Layout::new()
+            .title("Complete Elliptic Integral of the Legendre's Type (D)")
+            .x_axis(Axis::new().title("m").show_line(true))
+            .y_axis(
+                Axis::new()
+                    .title("D(m)")
+                    .show_line(true)
+                    .range(vec![0.0, 3.0]),
+            )
+            .annotations(vec![Annotation::new()
+            .text(format!(
+                "Generated using the function <a href=\"https://docs.rs/ellip/latest/ellip/legendre/fn.ellipd.html\" target=\"_blank\">ellipd</a> from <a href=\"https://crates.io/crates/ellip\" target=\"_blank\">ellip</a> v{}",
+                env!("CARGO_PKG_VERSION")
+            ))
+            .x_ref("paper")
+            .y_ref("paper")
+            .y(-0.15)
+            .x(1.05)
+            .show_arrow(false)]),
+    );
+
+    plot.show_html("figures/ellipd_plot.html");
+    plot.write_image("figures/ellipd_plot.svg", ImageFormat::SVG, 900, 600, 1.0);
+    Ok(())
+}
