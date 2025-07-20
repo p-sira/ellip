a9cbbf15122b091fc0f4125c2071d6217343f8d3
Sira Pornsiriprasert <code@psira.me>
2025-07-20 12:29:57 +0700

    CHG: Try grcov workflow

--- src/carlson/elliprd.rs
+++ src/carlson/elliprd.rs
@@ -162,7 +162,7 @@ pub fn elliprd<T: Float>(x: T, y: T, z: T) -> Result<T, StrErr> {
         }
     }
 
-    Err("elliprd: Failed to converge.")
+    Err("elliprd: Failed to converge.") // grcov-excl-line
 }
 
 const N_MAX_ITERATIONS: usize = 50;
