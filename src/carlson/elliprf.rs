f9135c6eec8251c513e5b0af91c666cbf15b7ce2
Sira Pornsiriprasert <code@psira.me>
2025-07-21 21:52:57 +0700

    DOC: Consolidating Boost Software License, remove history

--- src/carlson/elliprf.rs
+++ src/carlson/elliprf.rs
@@ -8,15 +8,7 @@
 //  Copyright (c) 2006 Xiaogang Zhang, 2015 John Maddock
 //  Copyright (c) 2024 Matt Borland
 //  Use, modification and distribution are subject to the
-//  Boost Software License, Version 1.0. (See accompanying file
-//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
-//  History:
-//  XZ wrote the original of this file as part of the Google
-//  Summer of Code 2006.  JM modified it to fit into the
-//  Boost.Math conceptual framework better, and to handle
-//  types longer than 80-bit reals.
-//  Updated 2015 to use Carlson's latest methods.
+//  Boost Software License, Version 1.0.
 
 use std::mem::swap;
 
--- src/legendre/ellippi.rs
+++ src/legendre/ellippi.rs
@@ -8,14 +8,7 @@
 //  Copyright (c) 2006 Xiaogang Zhang
 //  Copyright (c) 2006 John Maddock
 //  Use, modification and distribution are subject to the
-//  Boost Software License, Version 1.0. (See accompanying file
-//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
-//  History:
-//  XZ wrote the original of this file as part of the Google
-//  Summer of Code 2006.  JM modified it to fit into the
-//  Boost.Math conceptual framework better, and to correctly
-//  handle the various corner cases.
+//  Boost Software License, Version 1.0.
 
 use num_traits::Float;
 
