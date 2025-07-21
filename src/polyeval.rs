831d3704f04daf76f96895c2690686538fccde87
Sira Pornsiriprasert <code@psira.me>
2025-07-21 21:11:54 +0700

    DOC: polyeval: Minify the doc.

--- src/polyeval.rs
+++ src/polyeval.rs
@@ -4,53 +4,7 @@
  * This code is translated from SciPy C++ implementation to Rust.
  */
 
-/* Translated into C++ by SciPy developers in 2024.
- * Original header with Copyright information appears below.
- */
-
-/*                                                     polevl.c
- *                                                     p1evl.c
- *
- *     Evaluate polynomial
- *
- *
- *
- * SYNOPSIS:
- *
- * int N;
- * double x, y, coef[N+1], polevl[];
- *
- * y = polevl( x, coef, N );
- *
- *
- *
- * DESCRIPTION:
- *
- * Evaluates polynomial of degree N:
- *
- *                     2          N
- * y  =  C  + C x + C x  +...+ C x
- *        0    1     2          N
- *
- * Coefficients are stored in reverse order:
- *
- * coef[0] = C  , ..., coef[N] = C  .
- *            N                   0
- *
- * The function p1evl() assumes that c_N = 1.0 so that coefficent
- * is omitted from the array.  Its calling arguments are
- * otherwise the same as polevl().
- *
- *
- * SPEED:
- *
- * In the interest of speed, there are no checks for out
- * of bounds arithmetic.  This routine is used by most of
- * the functions in the library.  Depending on available
- * equipment features, the user may wish to rewrite the
- * program in microcode or assembly language.
- *
- */
+/* Translated into C++ by SciPy developers in 2024. */
 
 /*
  * Cephes Math Library Release 2.1:  December, 1988
@@ -63,17 +17,11 @@
  *     https://www.boost.org/doc/libs/1_61_0/libs/math/doc/html/math_toolkit/roots/rational.html
  */
 
-/* Scipy changes:
- * - 06-23-2016: add code for evaluating rational functions
- */
-
-//! Polynomial evaluation
-
 use num_traits::Float;
 
-/// Evaluate polynomial
+/// Evaluate polynomial with coefficients in reverse order (C_0 + C_1 x + C_2 x^2 + ...)
 #[inline]
-pub fn polyeval<T: Float>(x: T, coeff: &[T]) -> T {
+pub(crate) fn polyeval<T: Float>(x: T, coeff: &[T]) -> T {
     let mut ans = T::zero();
     coeff.iter().rev().for_each(|&k| ans = ans * x + k);
     ans
