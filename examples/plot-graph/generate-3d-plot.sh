ef80e125bd61dfd9380622ac400bf2c2aaf01a38
Sira Pornsiriprasert <code@psira.me>
2025-07-24 13:36:30 +0700

    DOC: Add 3D plot script

---
+++ examples/plot-graph/generate-3d-plot.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# List of 3D plot binaries
+BINARIES=(
+    "ellipdinc-3d"
+    "ellipeinc-3d"
+    "ellipf-3d"
+    "ellippiinc"
+)
+
+# Build and run each binary, waiting for user input
+for BIN in "${BINARIES[@]}"; do
+    echo "Running $BIN..."
+    cargo run --bin "$BIN" --features open-html
+    echo
+    read -p "Press Enter to continue to the next 3D plot..." dummy
+    echo
+done
+
+echo "All 3D plot binaries have been run."
